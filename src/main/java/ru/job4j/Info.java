668061d3b751420ff5c93d1f41bd10f7d2b9a66f
mazajaja <you@example.com>
2022-03-11 00:44:09 +0600

    6. Checkstyle   [#4123 #273825]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("11.03.2022");
+            System.out.println("11.03.2022");
     }
 }
