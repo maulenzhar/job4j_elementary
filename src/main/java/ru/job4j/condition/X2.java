c6e49aa28f964fdbc46d1a7174198c9f90176afb
mazajaja <you@example.com>
2022-04-04 01:55:59 +0600

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -6,11 +6,7 @@ public class X2 {
     }
 
     public static void main(String[] args) {
-//        int  a = 10, b = 0, c = 0, x = 2;
-//        int  a = 1, b = 1, c = 1, x = 1;
-//        int  a = 0, b = 1, c = 1, x = 1;
-//        int  a = 1, b = 1, c = 0, x = 1;
-        int  a = 1, b = 1, c = 1, x = 0;
+        int  a = 10, b = 0, c = 0, x = 2;
         int rsl = X2.calc(a, b, c, x);
         System.out.println(rsl);
     }
