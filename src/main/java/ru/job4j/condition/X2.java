e6cfcd8c5ded749800f840704853e1e30dbf7332
mzharylgasynov <$dbx254WSX>
2022-04-04 18:16:31 +0600

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -6,7 +6,10 @@ public class X2 {
     }
 
     public static void main(String[] args) {
-        int  a = 10, b = 0, c = 0, x = 2;
+        int  a = 10;
+        int  b = 0;
+        int  c = 0;
+        int  x = 2;
         int rsl = X2.calc(a, b, c, x);
         System.out.println(rsl);
     }
