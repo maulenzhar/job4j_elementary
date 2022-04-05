40a513bf0d71b2f17410fd91b91b8a21af582fe6
mazajaja <you@example.com>
2022-04-05 10:56:05 +0600

    Тернарное сравнение [#189 #284252]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -17,6 +17,15 @@ public class MaxTest {
     }
 
     @Test
+    public void whenMax3To2Then3() {
+        int left = 3;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
     public void whenMax3To3Then3() {
         int left = 3;
         int right = 3;
