2e591533241ae9a5816bf84afc7756df47e52a59
mazajaja <you@example.com>
2022-04-07 13:37:27 +0600

    5.5. Простое число [#171694]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -16,9 +16,4 @@ public class CheckPrimeNumber {
 
         return prime;
     }
-
-    public static void main(String[] args) {
-        System.out.println(CheckPrimeNumber.check(1));
-//        System.out.println(CheckPrimeNumber.check(4));
-    }
 }
