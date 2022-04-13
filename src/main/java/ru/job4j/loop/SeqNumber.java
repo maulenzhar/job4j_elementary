9bbb66d1e7ce9af753bd3f770ce0a8b5a53d42a3
mzharylgasynov <$dbx254WSX>
2022-04-13 18:00:32 +0600

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/loop/SeqNumber.java
+++ src/main/java/ru/job4j/loop/SeqNumber.java
@@ -2,12 +2,12 @@ package ru.job4j.loop;
 
 public class SeqNumber {
     public static void main(String[] args) {
-        calc(6);
-        calc(5);
-        calc(3);
+        calculate(6);
+        calculate(5);
+        calculate(3);
     }
 
-    public static void calc(int n) {
+    public static void calculate(int n) {
         int hold = 0;
         for (int index = 1; index <= n; index++) {
             hold += index;
